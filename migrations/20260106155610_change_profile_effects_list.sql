84b1328526c0f6310b112a513283d81a90bb88c8
Gabriel José <gabriel.josemelo@hotmail.com>
2026-01-06 16:09:54 -0300

    feat: add migration to change profile dice effects

---
+++ migrations/20260106155610_change_profile_effects_list.sql
@@ -0,0 +1,14 @@
+-- Up
+Update profiles
+Set redEffects = '{"type":"d20","effects":'
+  ||redEffects
+  ||'}',
+blueEffects = '{"type":"d20","effects":'
+  ||blueEffects
+  ||'}',
+blackEffects = '{"type":"d20","effects":'
+  ||blackEffects
+  ||'}'
+;
+
+-- Down
